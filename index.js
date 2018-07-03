78483b517cb6692216d234cfd00d438368ac1824
Balthild <ibalthild@gmail.com>
2018-07-03 11:51:32 +0800

    Init Chevereto plugin

--- index.js
+++ index.js
@@ -1,219 +1,47 @@
 var Package = require("./package.json");
 
-var AWS = require('aws-sdk'),
-    mime = require("mime"),
-    uuid = require("uuid").v4,
+var mime = require("mime"),
     fs = require('fs'),
     path = require('path'),
-
-  winston = module.parent.require('winston'),
-  db = module.parent.require('./database'),
-  templates = module.parent.require('./../public/src/templates');
-
+    request = require('request'),
+    nconf = module.parent.require('nconf'),
+    winston = module.parent.require('winston');
 
 (function(plugin) {
-  "use strict";
+    "use strict";
 
-  var S3Conn = null;
-  var settings = {
-    "accessKeyId": false,
-    "secretAccessKey": false,
-    "bucket": process.env.S3_UPLOADS_BUCKET || undefined
-  };
-
-  var accessKeyIdFromDb = false;
-  var secretAccessKeyFromDb = false;
-
-  function fetchSettings(){
-    db.getObjectFields(Package.name, Object.keys(settings), function(err, newSettings){
-      if(err) {
-        return winston.error(err);
-      }
-
-      accessKeyIdFromDb = false;
-      secretAccessKeyFromDb = false;
-
-      if(newSettings.accessKeyId){
-        settings.accessKeyId = newSettings.accessKeyId;
-        accessKeyIdFromDb = true;
-      }else{
-        settings.accessKeyId = false;
-      }
-
-      if(newSettings.secretAccessKey){
-        settings.secretAccessKey = newSettings.secretAccessKey;
-        secretAccessKeyFromDb = false;
-      }else{
-        settings.secretAccessKey = false;
-      }
-
-      if(!newSettings.bucket){
-        settings.bucket = process.env.S3_UPLOADS_BUCKET || "";
-      }else{
-        settings.bucket = newSettings.bucket;
-      }
-
-      if(settings.accessKeyId && settings.secretAccessKey){
-        AWS.config.update({
-          accessKeyId: settings.accessKeyId,
-          secretAccessKey: settings.secretAccessKey
-        });
-      }
-    });
-  }
-
-  function S3(){
-    if(!S3Conn){
-      S3Conn = new AWS.S3();
-    }
-
-    return S3Conn;
-  }
-
-  function makeError(err){
-    if(err instanceof Error){
-      err.message = Package.name + " :: " + err.message;
-    }else{
-      err = new Error(Package.name + " :: " + err);
-    }
-
-    winston.error(err);
-    return err;
-  }
-
-  // Delete settings on deactivate:
-  plugin.activate = function(){
-    fetchSettings();
-  };
-
-  // Delete settings on deactivate:
-  plugin.deactivate = function(){
-    S3Conn = null;
-  };
-
-  plugin.load = function(){
-    fetchSettings();
-  };
-
-  plugin.handleUpload = function (image, callback) {
-    if(!image || !image.path){
-      winston.error(image);
-      return callback(makeError("Invalid image data from plugin hook 'filter:uploadImage'"));
-    }
-
-    fs.readFile(image.path, putObject);
-
-    function putObject(err, buffer){
-      // Error from FS module:
-      if(err){
-        return callback(makeError(err));
-      }
-
-      var params = {
-        Bucket: settings.bucket,
-        ACL: "public-read",
-        Key: uuid() + path.extname(image.name),
-        Body: buffer,
-        ContentLength: buffer.length,
-        ContentType: mime.lookup(image.name)
-      };
-
-      S3().putObject(params, function(err){
-        if(err){
-          return callback(makeError(err));
+    function makeError(err){
+        if (err instanceof Error) {
+            err.message = Package.name + " :: " + err.message;
+        } else {
+            err = new Error(Package.name + " :: " + err);
         }
 
-        callback(null, {
-          name: image.name,
-          // Use protocol-less urls so that both HTTP and HTTPS work:
-          url: "//" + params.Bucket + ".s3.amazonaws.com/" + params.Key
-        });
-      });
+        winston.error(err);
+        return err;
     }
-  }
 
-
-  var admin = plugin.admin = {};
-  var adminRoute = "/plugins/s3-uploads";
-
-  admin.menu = function(headers) {
-    headers.plugins.push({
-      "route": adminRoute,
-      "icon": 'fa-picture-o',
-      "name": 'S3 Uploads'
-    });
-
-    return headers;
-  }
-
-  admin.route = function(pluginAdmin, callback) {
-    fs.readFile(path.join(__dirname, 'public/templates/admin.tpl'), function(err, tpl) {
-      if(err){
-        return callback(makeError(err), pluginAdmin);
-      }
-
-      pluginAdmin.routes.push({
-        route: adminRoute,
-        method: 'get',
-        options: function(req, res, next){
-          next({
-            req: req,
-            res: res,
-            route: adminRoute,
-            name: 'S3 Uploads',
-            content: templates.prepare(tpl.toString()).parse({
-              bucket: settings.bucket,
-              accessKeyId: (accessKeyIdFromDb && settings.accessKeyId) || "",
-              secretAccessKey: (accessKeyIdFromDb && settings.secretAccessKey) || ""
-            })
-          });
+    plugin.handleUpload = function (image, callback) {
+        if(!image || !image.path){
+            winston.error(image);
+            return callback(makeError("Invalid image data from plugin hook 'filter:uploadImage'"));
         }
-      });
 
-      pluginAdmin.api.push({
-        route: adminRoute + "/bucket",
-        method: 'post',
-        callback: function(req, res, next){
-          var data = req.body;
-          var newSettings = {
-            bucket: data.bucket || ""
-          }
-
-          db.setObject(Package.name, newSettings, function(err, res){
-            if(err){
-              winston.error(makeError(err));
-              return next({ error: true, message: err.toString() });
+        request.post({
+            url: nconf.get('chevereto:site_url') + '/api/1/upload/?key=' + nconf.get('chevereto:key'),
+            formData: {
+                source: fs.createReadStream(image.path),
+            }
+        }, function (err, resp, body) {
+            if (err) {
+                return callback(makeError(err));
             }
 
-            fetchSettings();
-            return next({ error: false, message: 'Saved!' });
-          });
-        }
-      });
-
-      pluginAdmin.api.push({
-        route: adminRoute + "/credentials",
-        method: 'post',
-        callback: function(req, res, next){
-          var data = req.body;
-          var newSettings = {
-            accessKeyId: data.accessKeyId || "",
-            secretAccessKey: data.secretAccessKey || ""
-          };
-
-          db.setObject(Package.name, newSettings, function(err, res){
-            if(err){
-              winston.error(makeError(err));
-              return next({ error: true, message: err.toString() });
-            }
-
-            fetchSettings();
-            return next({ error: false, message: 'Saved!' });
-          });
-        }
-      });
-
-      callback(null, pluginAdmin);
-    });
-  }
+            var data = JSON.parse(body);
+            callback(null, {
+                name: data.image.name,
+                url: data.image.url
+            });
+        });
+    }
 }(module.exports));
